b0542925085160dfa0bbcab1ecc7afceaa592ba2
HMuraja <hilla.muraja@hotmail.com>
2025-03-02 19:11:21 +0000

    Style added

--- about-dogs/src/App.jsx
+++ about-dogs/src/App.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
-import viteLogo from '/vite.svg'
 import DogBreed from './components/DogBreed'
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDog } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   const [dogBreeds, setdogBreeds] = useState([]);
+  
 
   const breedsEndpoint = 'https://dogapi.dog/api/v2/breeds';
 
@@ -32,23 +34,30 @@ function App() {
     assignBreeds();
   }, []);
 
+  const mapBreeds = () => {
+      let colorIndex = 0;
+      return dogBreeds.map((breed, index) => {
+        colorIndex == 3 ? colorIndex=0 : colorIndex++;
+        return <DogBreed breed={breed} key={index} indx={colorIndex}/>
+      });
+  }
+
   return (
-    <>
-      <div className='flex justify-center items-center '>
-        
-        <img src={viteLogo}  alt="Vite logo" />
+    <div className="p-10">
+      <div className='flex py-2'>
+        <FontAwesomeIcon icon={faDog} className="text-rose-c font-bold text-6xl pr-3"/>
+        <h1 className="text-rose-c font-theme font-bold two text-6xl tracking-wider uppercase">Dog Facts
+        </h1>
       </div>
-      <h1 className="text-blue-600 two text-2xl tracking-wider uppercase">Dog Facts</h1>
+      <hr className="text-rose-c border-4 rounded-lg text-center"/>
+      
       <div>
-        <h2>Available Dog Breeds</h2>
-        <div>
-          {dogBreeds.length != 0 &&
-          dogBreeds.map((breed, index) => (
-            <DogBreed breed={breed} key={index}/>
-          ))}
+        <h2 className="text-white font-theme font-bold text-2xl tracking-widest my-2 py-2 rounded-4xl flex justify-center bg-rose-c uppercase">The Dog Breeds</h2>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+          {dogBreeds.length !== 0 && mapBreeds()}
         </div>
       </div>
-   </>
+   </div>
   )
 }
 
---
+++ about-dogs/src/components/BreedsList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BreedsList() {
+  return (
+    <div>BreedsList</div>
+  )
+}
+
+export default BreedsList
