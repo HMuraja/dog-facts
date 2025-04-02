4ce95b04702a9a25ce7a59783b9c4b2f18eb4d54
hilla_muraja <hilla.muraja@hotmail.com>
2025-04-02 22:03:25 +0100

    update vite.config

--- about-dogs/vite.config.js
+++ about-dogs/vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: "/dog-facts/",
+  base: "/dog-facts",
 })
 
